35cae768e1169fc9520c90551cae8ed05a744aeb
Eric Chen <eric@ericnchen.com>
2019-08-16 09:39:23 +0200

    Refactor skeleton/outline w/ real functionality.
    - Pushcut messages now will be correctly sent.
    - AWS Lambda response should be correct (but not tested/deployed yet).
    - Refactored some repeated code into reusable bits (not fully done).
    
    In theory, I should be able to deploy this as soon as I write the
    Serverless config for it/

--- handler.py
+++ handler.py
@@ -1,61 +1,109 @@
 # -*- coding: utf-8 -*-
 import base64
-import datetime as dt
+import datetime
 import json
+import logging
 import os
 import pathlib
 
 import requests
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 # shim env; won't be needed in real deployment
 for k, v in json.load(pathlib.Path("credentials.json").open(encoding="utf-8")).items():
     os.environ[k.upper()] = v
 
 
-def main():
-    tg_url = "https://www.toggl.com/api/v8/time_entries/current"
-    tg_key = os.environ["TOGGL_API_TOKEN"]
-    tg_akey = base64.b64encode(f"{tg_key}:api_token".encode("utf-8")).decode("utf-8")
-    tg_resp = requests.get(tg_url, headers={"Authorization": f"Basic {tg_akey}"})
-    tg_body = tg_resp.json()
-    tg_data = tg_body["data"]
+class PushcutHandler:
+    def __init__(self, token: str) -> None:
+        self._auth_token = token
 
-    if tg_data:
-        dur_threshold = 5  # 60  # Minutes
-        dur = int((dt.datetime.now().timestamp() + tg_data["duration"]) / 60)
-        if dur > dur_threshold:
-            pc_url = "https://api.pushcut.io/v1/notifications/Hello"
-            pc_token = os.environ["PUSHCUT_API_TOKEN"]
+    def push_notification(self, **kwargs):
+        r = requests.post(
+            "https://api.pushcut.io/v1/notifications/Hello",
+            json=kwargs,
+            headers={"API-Key": self._auth_token},
+        )
+        logger.debug(f"Pushcut: {r.json()['message']}")
+        return r
+
+
+class TogglHandler:
+    def __init__(self, token: str) -> None:
+        self._auth_token = base64.b64encode(f"{token}:api_token".encode()).decode()
+
+    def get_current_entry(self):
+        r = requests.get(
+            "https://www.toggl.com/api/v8/time_entries/current",
+            headers={"Authorization": "Basic " + self._auth_token},
+        )
+        data = r.json()["data"]
+        return TogglEntry(data)
+
+
+class TogglEntry:
+    def __init__(self, data: dict) -> None:
+        self._data = data
+
+    @property
+    def description(self) -> str:
+        """Description of the entry."""
+        return self._data["description"]
+
+    @property
+    def duration(self) -> int:
+        """Duration of the entry in minutes."""
+        return int((datetime.datetime.now().timestamp() + self._data["duration"]) / 60)
+
+
+def lambda_function(event, context):
+    toggl = TogglHandler(os.environ["TOGGL_API_TOKEN"])
+    entry = toggl.get_current_entry()
+
+    try:
+        duration = entry.duration
+        threshold = 5  # 60  # Minutes
+        if duration > threshold:
+            logger.debug(f"Duration threshold met ({duration} > {threshold})")
             pc_body = {
-                "text": f"{tg_data['description']} has been running for {dur} minutes.",
-                "title": "Toggl Watcher",
+                "text": f"{entry.description} has been running for {duration} minutes.",
                 "actions": [
-                    {"name": "keep it running", "url": "http://localhost"},
-                    {"name": "stop the timer", "url": "http://localhost"},
+                    {"name": "Stop timer", "shortcut": "Stop Current Toggl Timer"},
+                    {"name": "Do nothing"},
                 ],
             }
-            pc_resp = requests.post(pc_url, json=pc_body, headers={"API-Key": pc_token})
-            if pc_resp.status_code == 200:
-                return  # message = success
-            return  # message = failure
         else:
-            return  # exit early w/ some kind of status or message
-    else:
-        pc_url = "https://api.pushcut.io/v1/notifications/Hello"
-        pc_token = os.environ["PUSHCUT_API_TOKEN"]
+            logger.debug(f"Duration threshold not met ({duration} < {threshold})")
+            return {
+                "headers": {"Content-Type": "application/json"},
+                "isBase64Encoded": False,
+                "statusCode": 200,
+                "body": json.dumps({}),
+            }
+
+    except TypeError:
+        logger.debug("No Toggl entry")
         pc_body = {
-            "text": "Time isn't being logged right now.",
-            "title": "Toggl Watcher",
+            "text": "Tap to open Toggl or pull down for more options.",
+            "defaultAction": {"name": "Open Toggl", "url": "toggl://"},
             "actions": [
-                {"name": "track something", "url": "http://localhost"},
-                {"name": "don't track anything", "url": "http://localhost"},
+                {"name": "Open Toggl", "url": "toggl://"},
+                {"name": "Do nothing"},
             ],
         }
-        pc_resp = requests.post(pc_url, json=pc_body, headers={"API-Key": pc_token})
-        if pc_resp.status_code == 200:
-            return  # message = success
-        return  # message = failure
+
+    ph = PushcutHandler(os.environ["PUSHCUT_API_TOKEN"])
+    r = ph.push_notification(title="Toggl Watcher", **pc_body)
+
+    return {
+        "headers": {"Content-Type": "application/json"},
+        "isBase64Encoded": False,
+        "statusCode": r.status_code,
+        "body": json.dumps(r.json()),
+    }
 
 
 if __name__ == "__main__":
-    main()
+    lambda_function(None, None)
